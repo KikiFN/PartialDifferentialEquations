formula at t=1 now works properly. Norm is correct

leapfrog2D.py
@@ -12,7 +12,6 @@ L=10
 J=51
 I=51
 cf= 0.5
-norme = []
 deltax= L/(J-1)
 deltay= L/(I-1)
 deltat= (0.5*deltax)/v
@@ -34,8 +33,18 @@ def whichMinMax(j):
         umin=j-1
         umax=j+1
     return umin,umax
+
 def norma(*y):
     return LA.norm(y)/np.sqrt(J)
+
+'''def norma(*y):
+    somma=0
+    for j in range(0,len(y)):
+        for el in y[j]:
+            mod=0
+            mod = np.power(el,2)
+            somma+=mod
+    return np.sqrt(somma/J)'''
 #MAIN---------------------------------------------
 #calcolo vettore x
 x_val= np.arange(0,L,deltax)
@@ -50,27 +59,26 @@ u0 = np.zeros([lenX,lenY])
 u_step = np.zeros([lenX,lenY])
 u = np.empty([lenT,lenX,lenY])
 
-u0= gaussian0(X,Y)
-u[1]= gaussian(X,Y)
+u[0]= gaussian0(X,Y)
+#u[1]= gaussian(X,Y)
 
 for t in range(lenT):
-    
     for x in range(lenX):
         xmin,xmax = whichMinMax(x)
         for y in range(lenY):
             ymin,ymax = whichMinMax(y)
             if t ==0:
-                u_step[x,y] = u0[x,y]
+                u_step[x,y] = u[0,x,y]
                 #print(u_step[x,y], u0[x,y])
             elif t == 1:
-                u_step[x,y] = u[1,x,y]
+                u_step[x,y] = 2*u[t-1,x,y] - u[0,x,y] + cost*(u[t-1,xmax,y] - 2*u[t-1,x,y] + u[t-1,xmin,y]) + cost*(u[t-1,x,ymax] - 2*u[t-1,x,y] + u[t-1,x,ymin])
             else:
                 u_step[x,y] = 2*u[t-1,x,y] - u[t-2,x,y] + cost*(u[t-1,xmax,y] - 2*u[t-1,x,y] + u[t-1,xmin,y]) + cost*(u[t-1,x,ymax] - 2*u[t-1,x,y] + u[t-1,x,ymin])
-    #print(u_step)
     u[t] = u_step
-    norme[t] = norma(u_step)
+    
 
-#print(u[0],u0)
+norme = [norma(ut) for ut in u]
+    
 
 permitted_times = {
     0:0,
@@ -80,30 +88,28 @@ permitted_times = {
     200:20
 }
 
-fig = plt.figure(1)
-ax = fig.gca(projection='3d')
-#x_val, y_val = np.meshgrid(x_val, y_val)
-ax = plt.axes(projection='3d')
-ph=1
-for i,Z in enumerate(u):
-    if i in permitted_times.keys():
-        #plt.cla()
-        surf = ax.plot_surface(X,Y,Z,label='u(x,'+ str(permitted_times[i]) +')',alpha=ph)
-        surf._facecolors2d=surf._facecolors3d
-        surf._edgecolors2d=surf._edgecolors3d
-        ph-=0.2
-ax.legend(loc=0)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
-#plt.savefig('leapfrog2D_frame{}.png'.format(permitted_times[i]))
-plt.show(block=True)
-
-fig2=plt.figure(2)
+fig2=plt.figure(1)
 plt.plot(t_val,norme)
 plt.title('Norma Leapfrog 2D')
 plt.xlabel('t')
 plt.ylabel('Leap2D-norm')
-#fig2.set_size_inches(10,7)
 #plt.savefig("normeleap2D.png", dpi=100)
 plt.show(block=True)
+
+for i,Z in enumerate(u):
+    n=2
+    if i in permitted_times.keys():
+        fig = plt.figure(n)
+        ax = fig.gca(projection='3d')
+        ax = plt.axes(projection='3d')
+        plt.cla()
+        surf = ax.plot_surface(X,Y,Z,label='u(x,'+ str(permitted_times[i]) +')')
+        surf._facecolors2d=surf._facecolors3d
+        surf._edgecolors2d=surf._edgecolors3d
+        ax.legend(loc=0)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        plt.savefig('leapfrog2D_frame{}.png'.format(permitted_times[i]))
+        #plt.show(block=True)
+    n+=1