e3e018e542c1784597b87e25760bf2236df61306
giada24 <giada4r@yahoo.it>
2020-06-22 17:12:28 +0200

    new version

--- laxfriedrichs.py
+++ laxfriedrichs.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import linalg as LA
 
-#VALORI DI INPUZZ ---------------------------------
+#VALORI DI INPUT ---------------------------------
 a=1
 x0=5
 L=10
@@ -9,12 +10,16 @@ J=101
 cf= 0.5
 u = []
 un,un2,norme = [],[],[]
+err=[]
 deltax= L/(J-1)
 deltat= (deltax * cf)/a
 
 #FUNZIONI-----------------------------------------
 def gaussian(a, b):
     return np.exp(-np.power(a-b,2))
+    
+def analytical(a,b,t):
+    return np.exp(-np.power(a-t-b,2))
 
 def laxfried(*x):
     y = []
@@ -29,50 +34,51 @@ def laxfried(*x):
             else:
                 umin=j-1
                 umax=j+1
-
         val= 0.5*( (x[umin]) + (x[umax]) ) - ( (a*deltat)/(2*deltax) )*( (x[umax]) - (x[umin]) )
         y.append(val)
     return y
 
 def norma(*y):
-    somma=0
-    for j in range(0,len(y)):
-        mod=0
-        mod = np.power(y[j],2)
-        somma+=mod
-    return np.sqrt(somma/J)
+    return LA.norm(y)/np.sqrt(J)
 #--------------------------------------------------
+x_val= np.arange(0,L,deltax)  #calcolo vettore x 
 
-#calcolo vettore x
-x_val= np.arange(0,L,deltax) 
-
-plt.figure(1)
-#primo u
+fig1=plt.figure(1)
 for i in x_val:
-    u.append(gaussian(i,x0)) #calcolo vettore u
-plt.plot(x_val,u)
-norme.append(norma(*u))
-#secondo u
-un = laxfried(*u)
-plt.plot(x_val,un)
-norme.append(norma(*un))
+    u.append(gaussian(i,x0))  #calcolo vettore u
+plt.plot(x_val,u, label='u(x,0)')
+un = laxfried(*u)             #secondo u
 
+permitted_times = {
+    100:5,
+    200:10,
+    300:15,
+    400:20
+}
 #tutti gli altri u
-for n in np.arange(0,20+deltat,deltat):
+for i,n in enumerate(np.arange(0,20+deltat,deltat)):
     un2 = laxfried(*un)
     norme.append(norma(*un2))
-    #print(un2)
-    plt.plot(x_val,un2)
+    if i in permitted_times.keys(): 
+        err.append(norma(un2 - analytical(x_val,x0,i)))
+        plt.plot(x_val,un2,label='u(x,'+ str(permitted_times[i]) +')')
     un = un2
-    n+=1
+plt.legend(loc=0)  
+plt.xlabel('x')
+plt.ylabel('u')
+plt.title('Metodo Lax-Friedrichs')
+fig1.set_size_inches(10,7)
+plt.savefig("2_lax-friedrichs.png", dpi=100)
 
+fig2=plt.figure(2)
+plt.plot(np.arange(0,20+deltat,deltat),norme)
+plt.title('Norma l2 con metodo Lax-Friedrichs')
+plt.xlabel('t')
+plt.ylabel('l2-norm')
+fig2.set_size_inches(10,7)
+plt.savefig("2_norme.png", dpi=100)
 
-plt.savefig("lax-friedrichs.png")
-plt.show(1)
-
-print(len(norme))
-
-plt.figure(2)
-plt.plot(norme)
-plt.savefig("norme.png")
-plt.show(2)
+plt.figure(3)
+for t,e in zip((permitted_times.values()),err):
+    plt.plot(t,1/e,marker='.')
+plt.savefig('2_err.png')
--- laxwendroff.py
+++ laxwendroff.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import linalg as LA
 
-#VALORI DI INPUZZ ---------------------------------
+#VALORI DI INPUT ---------------------------------
 a=1
 x0=5
 L=10
@@ -9,12 +10,16 @@ J=101
 cf= 0.5
 u = []
 un,un2,norme = [],[],[]
+err=[]
 deltax= L/(J-1)
 deltat= (deltax * cf)/a
 
 #FUNZIONI-----------------------------------------
 def gaussian(a, b):
     return np.exp(-np.power(a-b,2))
+
+def analytical(a,b,t):
+    return np.exp(-np.power(a-t-b,2))
 
 def laxwendroff(*x):
     y = []
@@ -29,50 +34,55 @@ def laxwendroff(*x):
             else:
                 umin=j-1
                 umax=j+1
-
-        val= x[j]-(((a*deltat)/(2*deltax))*(x[umax]-x[umin]))+(((np.power(a,2)*np.power(deltat,2)/(2*np.power(deltax,2)))*(x[umax]-(2*x[j])+x[umin])))
+        val= x[j]-(((a*deltat)/(2*deltax))*(x[umax]-x[umin]))+((np.power(a,2)*np.power(deltat,2))/(2*np.power(deltax,2)))*(x[umax]-(2*x[j])+x[umin])
         y.append(val)
     return y
 
 def norma(*y):
-    somma=0
-    for j in range(0,len(y)):
-        mod=0
-        mod = np.power(y[j],2)
-        somma+=mod
-    return np.sqrt(somma/J)
+    return LA.norm(y)/np.sqrt(J)
 #--------------------------------------------------
+x_val= np.arange(0,L,deltax)    #calcolo vettore x
 
-#calcolo vettore x
-x_val= np.arange(0,L,deltax) 
-
-plt.figure(1)
-#primo u
+fig1=plt.figure(1)
 for i in x_val:
-    u.append(gaussian(i,x0)) #calcolo vettore u
-plt.plot(x_val,u)
-norme.append(norma(*u))
-#secondo u
-un = laxwendroff(*u)
-plt.plot(x_val,un)
-norme.append(norma(*un))
+    u.append(gaussian(i,x0))    #calcolo vettore u
+plt.plot(x_val,u, label='u(x,0)')
+un = laxwendroff(*u)            #secondo u
 
+permitted_times = {
+    100:5,
+    200:10,
+    300:15,
+    400:20
+}
 #tutti gli altri u
-for n in np.arange(0,20+deltat,deltat):
+for i,n in enumerate(np.arange(0,20+deltat,deltat)):
     un2 = laxwendroff(*un)
     norme.append(norma(*un2))
-    #print(un2)
-    plt.plot(x_val,un2)
+    if i in permitted_times.keys():
+        err.append(norma(un2 - analytical(x_val,x0,i))) 
+        plt.plot(x_val,un2,label='u(x,'+ str(permitted_times[i]) +')')         
     un = un2
-    n+=1
+plt.legend(loc=0)
+plt.xlabel('x')
+plt.ylabel('u')
+plt.title('Metodo Lax-Wendroff')
+fig1.set_size_inches(10,7)
+plt.savefig("4_lax-wendroff.png", dpi=100)
 
-
-plt.savefig("lax-wendroff.png")
-plt.show(1)
-
-print(len(norme))
-
-plt.figure(2)
-plt.plot(norme)
-plt.savefig("normewendroff.png")
-plt.show(2)
+fig2=plt.figure(2)
+plt.plot(np.arange(0,20+deltat,deltat),norme)
+plt.title('Norma l2 con metodo Lax-Wendroff')
+plt.xlabel('t')
+plt.ylabel('l2-norm')
+fig2.set_size_inches(10,7)
+plt.savefig("4_normewendroff.png", dpi=100)
+'''
+plt.figure(3)
+for t,e in zip((permitted_times.values()),err):
+    plt.plot(t,e,'o-')
+plt.savefig('4_err.png')
+'''
+plt.figure(3)
+plt.plot(err,'o-')
+plt.savefig('4_err.png')
