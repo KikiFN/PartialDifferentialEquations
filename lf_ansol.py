abe0e004b77d5e21f9428fcb4751f0b188a1a11a
Francesca Negri <fnegri910@gmail.com>
2020-06-22 18:13:14 +0200

    testing on analytical solutions

--- lf_ansol.py
+++ lf_ansol.py
@@ -44,10 +44,10 @@ def norma(*y):
 x_val= np.arange(0,L,deltax)  #calcolo vettore x 
 
 fig1=plt.figure(1)
-for i in x_val:
-    u.append(gaussian(i,x0))  #calcolo vettore u 0
-#plt.plot(x_val,u, label='u(x,0)')
-un = laxfried(*u)             #u 1
+
+u = gaussian(x_val,x0)
+plt.plot(x_val,u,label='0')
+analytical_solutions = []
 
 permitted_times = {
     100:5,
@@ -56,40 +56,17 @@ permitted_times = {
     400:20
 }
 
-err = []
+for val in permitted_times.values():
+    analytical_solutions.append(analytical(x_val,x0,val))
 
-#tutti gli altri u
-for i,n in enumerate(np.arange(0,20+deltat,deltat)):
-    un2 = laxfried(*un) #u 2 in poi
-    norme.append(norma(*un2))
-    analytical_solution = analytical(x_val,x0,0)
-    if i in permitted_times.keys(): 
-        if i == 200 or i == 400: err.append(norma(un2 - analytical_solution))
-        plt.plot(x_val,un2,label='u(x,'+ str(permitted_times[i]) +')')
-    un = un2
+analytical_solutions = np.asarray(analytical_solutions)
+
+for i in range(len(analytical_solutions)):
+        plt.plot(x_val, analytical_solutions[i],label=str(5*(i+1)))
 plt.legend(loc=0)  
 plt.xlabel('x')
 plt.ylabel('u')
-plt.title('Metodo Lax-Friedrichs')
+plt.title('Sol. analitiche Lax-Friedrichs')
 #fig1.set_size_inches(10,7)
-plt.savefig("2_lax-friedrichs.png", dpi=100)
-plt.show(block=True)
-
-fig2=plt.figure(2)
-plt.plot(np.arange(0,20+deltat,deltat),norme)
-plt.title('Norma l2 con metodo Lax-Friedrichs')
-plt.xlabel('t')
-plt.ylabel('l2-norm')
-#fig2.set_size_inches(10,7)
-plt.savefig("2_norme.png", dpi=100)
-plt.show(block=True)
-
-permitted_times = {
-    200:10,
-    400:20
-}
-
-print(err)
-
-plt.plot(list(permitted_times.values()),err,marker='.')
+#plt.savefig("2_lax-friedrichs.png", dpi=100)
 plt.show(block=True)
